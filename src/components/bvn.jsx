1b64b196e006df3481010252dd749f007c70d585
JamiuKolawole04 <Jakko4real@gmail.com>
2023-07-15 01:34:38 +0100

    chore: integrating verify bvn api

--- src/components/bvn.jsx
+++ src/components/bvn.jsx
@@ -1,13 +1,101 @@
-import React from "react";
+import React, { useState } from "react";
+import { Navigate } from "react-router-dom";
+import axios from "axios";
 
 import { useAuthContext } from "../context/authContext";
-import { Navigate } from "react-router-dom";
 
 export const Bvn = () => {
+  const proxyUrl = "https://cors-anywhere.herokuapp.com/";
+  const baseUrl = "https://api.sandbox.youverify.co/v2/api/identity/ng/bvn";
   const { user } = useAuthContext();
+
+  const [bvn, setBvn] = useState("");
+  const [bvnData, setBvnData] = useState({});
+  const [isBvnLoading, setIsBvnLoading] = useState(false);
+  //   found;
+  //   not_found
+
+  console.log({ bvnData });
 
   if (!user) {
     return <Navigate replace to="/login" />;
   }
-  return <div>Bvn</div>;
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      //   const response = await fetch(`${proxyUrl}${baseUrl}`, {
+      //     method: "POSTs",
+      //     headers: {
+      //       "Content-Type": "application/json",
+      //       token: `SXFn2GA8.HwmyddDZkgmSdODrmtkHu1TwqPpagnKZ5PPE`,
+      //       "Access-Control-Allow-Origin": "*",
+      //     },
+      //     body: JSON.stringify({
+      //       id: bvn,
+      //       isSubjectConsent: true,
+      //       premiumBVN: true,
+      //     }),
+      //   });
+      //   console.log({ response });
+
+      setIsBvnLoading(true);
+
+      const { data } = await axios({
+        method: "POST",
+        // url: "https://api.sandbox.youverify.co/v2/api/identity/ng/bvn",
+        // url: "https://cors-anywhere.herokuapp.com/https://api.sandbox.youverify.co/v2/api/identity/ng/bvn",
+        url: `${proxyUrl}${baseUrl}`,
+        headers: {
+          token: "SXFn2GA8.HwmyddDZkgmSdODrmtkHu1TwqPpagnKZ5PPE",
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          Origin: "https://api.sandbox.youverify.co/v2/api/identity/ng/bvn",
+        },
+        data: {
+          id: bvn,
+          isSubjectConsent: true,
+          premiumBVN: true,
+        },
+      });
+      setBvnData(data.data);
+      return data;
+    } catch (err) {
+      console.log({ err });
+    } finally {
+      setIsBvnLoading(false);
+    }
+  };
+
+  return (
+    <form
+      className="flex justify-center items-center flex-col mt-20 w-full m-auto max-w-xs"
+      onSubmitCapture={handleSubmit}
+    >
+      <h1 className="text-white font-bold mb-5">Enter bvn to verify </h1>
+
+      <input
+        type="text"
+        maxLength="11"
+        name="bvn"
+        id="bvn"
+        className="w-full mb-8"
+        placeholder="bvn"
+        value={bvn}
+        onChange={({ target: { value } }) => setBvn(value)}
+      />
+
+      <button
+        type="submit"
+        className="bg-green-400 w-full py-2 rounded  disabled:cursor-not-allowed disabled:opacity-30"
+        disabled={bvn.length < 11}
+      >
+        {isBvnLoading ? "verifying..." : "verify"}
+      </button>
+      {!bvnData?.dataValidation && (
+        <p className="text-white">{bvnData?.reason}</p>
+      )}
+    </form>
+  );
 };
